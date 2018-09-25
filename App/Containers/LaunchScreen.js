125b2af9b5f59717eb36c7050f31eb8a04ad92f0
eguona <kkkkkkkkkkk kkkkkkkk@lorenc.eguona>
2018-09-25 08:40:33 +0200

    switch currencies and animated marker

--- App/Containers/LaunchScreen.js
+++ App/Containers/LaunchScreen.js
@@ -6,17 +6,23 @@ import {
   ScrollView,
   TouchableOpacity,
   TextInput,
-  Keyboard
+  Keyboard,
+  Animated,
+  Easing
 } from 'react-native';
 import TextTicker from 'react-native-text-ticker';
 import axios from 'axios';
 import 'intl';
 import 'intl/locale-data/jsonp/en';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import currencies from './currencies';
 
 // Styles
 import styles from './Styles/LaunchScreenStyles';
 import { Metrics, Colors } from '../Themes';
+
+const AnimatedIcon = Animated.createAnimatedComponent(Icon);
+const AnimatedTouchableOpacity = Animated.createAnimatedComponent(TouchableOpacity);
 
 export default class LaunchScreen extends Component {
   constructor(props) {
@@ -27,6 +33,8 @@ export default class LaunchScreen extends Component {
       target: 'USD',
       convert: false,
       text: '',
+      openMenu: false,
+      active: 'base',
       showFeed: true,
       textLength: 13,
       date: new Date()
@@ -34,15 +42,22 @@ export default class LaunchScreen extends Component {
         .slice(0, 10)
         .replace(/-/g, '-'),
       time: new Date().toLocaleTimeString().replace('/.*(d{2}:d{2}:d{2}).*/', '$1'),
-      rates: {}
+      rates: {},
+      feedRates: {}
     };
 
     this.getRates = this.getRates.bind(this);
+    this.onChange = this.onChange.bind(this);
+    this.animate = this.animate.bind(this);
+    this.animateMenu = this.animateMenu.bind(this);
+
+    this.activeAnim = new Animated.Value(0);
+    this.menuAnim = new Animated.Value(0);
   }
 
   componentDidMount() {
     axios.get(`https://api.exchangeratesapi.io/latest?base=${this.state.base}`).then(response => {
-      this.setState({ rates: response.data.rates });
+      this.setState({ rates: response.data.rates, feedRates: response.data.rates });
       this.tickerRef.startAnimation();
     });
     //this.interval = setInterval(
@@ -54,22 +69,130 @@ export default class LaunchScreen extends Component {
     //);
   }
 
-  componentWillUnmount() {
+  /*   componentWillUnmount() {
     clearInterval(this.interval);
+  } */
+
+  onChange(value) {
+    this.textInput.setNativeProps({ text: value });
+    this.setState({ text: value });
   }
 
   getRates() {
     axios.get(`https://api.exchangeratesapi.io/latest?base=${this.state.base}`).then(response => {
-      this.setState({ rates: response.data.rates, showFeed: true });
+      this.setState({ rates: response.data.rates, convert: true });
+    });
+  }
+
+  animate(move) {
+    Animated.timing(this.activeAnim, {
+      toValue: move ? 1 : 0,
+      duration: 200,
+      easing: Easing.out(Easing.back(0.01)),
+      useNativeDriver: true
+    }).start(() => {
+      this.setState({ active: move ? 'target' : 'base' });
+    });
+  }
+
+  animateMenu(move) {
+    Animated.timing(this.menuAnim, {
+      toValue: move ? 1 : 0,
+      duration: 200,
+      easing: Easing.out(Easing.back(0.01)),
+      useNativeDriver: true
+    }).start(() => {
+      this.setState({ openMenu: !this.state.openMenu });
     });
   }
 
   render() {
     const { base, target, rates, convert, text, textLength, showFeed } = this.state;
+    const activeCurrency = this.state.active;
+
+    const translateY = this.activeAnim.interpolate({
+      inputRange: [0, 1],
+      outputRange: [Metrics.screenHeight * 0.259, Metrics.screenHeight * 0.385],
+      extrapolate: 'clamp'
+    });
+
+    const flip = this.menuAnim.interpolate({
+      inputRange: [0, 1],
+      outputRange: ['0deg', '180deg'],
+      extrapolate: 'clamp'
+    });
+
+    const drop = this.menuAnim.interpolate({
+      inputRange: [0, 1],
+      outputRange: ['0deg', '180deg'],
+      extrapolate: 'clamp'
+    });
+
     return (
       <View style={styles.background}>
         <StatusBar translucent hidden />
+        <TouchableOpacity
+          activeOpacity={1}
+          style={{
+            height: Metrics.screenHeight * 0.06,
+            width: Metrics.screenHeight * 0.06,
+            borderRadius: Metrics.screenHeight * 0.15,
+            position: 'absolute',
+            //backgroundColor: 'white',
+            top: Metrics.screenHeight * 0.02,
+            right: Metrics.screenHeight * 0.02,
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}
+          onPress={() => {
+            this.animateMenu(!this.state.openMenu);
+          }}
+        >
+          <AnimatedIcon
+            name="menu-down"
+            size={40}
+            color="red"
+            style={{ elevation: 5, transform: [{ rotate: flip }] }}
+          />
+        </TouchableOpacity>
+        {/* <View
+          style={{
+            height: Metrics.screenHeight * 0.2,
+            width: Metrics.screenWidth,
+            backgroundColor: '#282828',
+            top: +Metrics.screenHeight * 0.4
+          }}
+        /> */}
         <Text style={styles.titleText}>Kōkan Rates</Text>
+        <AnimatedIcon
+          name="rhombus"
+          size={20}
+          color="#00FF7B"
+          style={{
+            position: 'absolute',
+            left: Metrics.screenWidth * 0.1,
+            transform: [{ translateY }]
+          }}
+        />
+        <TouchableOpacity
+          style={{
+            height: Metrics.screenHeight * 0.04,
+            width: Metrics.screenWidth * 0.15,
+            borderRadius: Metrics.screenWidth * 0.08,
+            position: 'absolute',
+            top: Metrics.screenHeight * 0.32,
+            left: Metrics.screenHeight * 0.135,
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}
+          onPress={() => {
+            this.setState({ base: target, target: base, convert: false }, () => {
+              this.getRates();
+            });
+          }}
+        >
+          <Icon name="undo-variant" size={18} color="white" />
+        </TouchableOpacity>
         <TextTicker
           ref={ref => (this.tickerRef = ref)}
           style={styles.scrollText}
@@ -79,8 +202,8 @@ export default class LaunchScreen extends Component {
           repeatSpacer={0}
         >
           {showFeed
-            ? Object.keys(this.state.rates).map(key => {
-                return `${this.state.base}-${key} ${this.state.rates[key]}  |  `;
+            ? Object.keys(this.state.feedRates).map(key => {
+                return `${'EUR'}-${key} ${this.state.feedRates[key]}  |  `;
               })
             : ' '}
         </TextTicker>
@@ -101,15 +224,27 @@ export default class LaunchScreen extends Component {
               alignItems: 'center'
             }}
           >
-            <Text
+            <TouchableOpacity
               style={{
-                color: 'white',
-                fontSize: 22,
-                fontFamily: 'SpaceMono-Regular'
+                height: Metrics.screenHeight * 0.08,
+                width: Metrics.screenWidth * 0.12,
+                justifyContent: 'center',
+                alignItems: 'center'
+              }}
+              onPress={() => {
+                this.animate(false);
               }}
             >
-              {this.state.base}
-            </Text>
+              <Text
+                style={{
+                  color: 'white',
+                  fontSize: 22,
+                  fontFamily: 'SpaceMono-Regular'
+                }}
+              >
+                {this.state.base}
+              </Text>
+            </TouchableOpacity>
             <View
               style={{
                 height: Metrics.screenHeight * 0.08,
@@ -122,6 +257,7 @@ export default class LaunchScreen extends Component {
               }}
             >
               <TextInput
+                ref={component => (this.textInput = component)}
                 maxLength={textLength}
                 keyboardType="numeric"
                 placeholder="Enter Value"
@@ -138,26 +274,31 @@ export default class LaunchScreen extends Component {
                   textAlign: 'center',
                   fontFamily: 'monospace'
                 }}
-                onChangeText={value =>
-                  this.setState({
-                    text: value
-                  })
-                }
-                value={text}
+                onChangeText={value => this.onChange(value)}
                 onFocus={() => {
-                  this.setState({
-                    convert: false,
-                    textLength: 13,
-                    text: text.replace(/(\d+),(?=\d{3}(\D|$))/g, '$1')
-                  });
+                  this.setState(
+                    {
+                      convert: false,
+                      textLength: 13,
+                      text: text.replace(/(\d+),(?=\d{3}(\D|$))/g, '$1')
+                    },
+                    () => {
+                      this.textInput.setNativeProps({ text: this.state.text });
+                    }
+                  );
                 }}
                 onSubmitEditing={() => {
                   Keyboard.dismiss();
-                  this.setState({
-                    convert: text !== '',
-                    textLength: 17,
-                    text: text !== '' ? parseFloat(text).toLocaleString('en') : ''
-                  });
+                  this.setState(
+                    {
+                      convert: text !== '',
+                      textLength: 17,
+                      text: text !== '' ? parseFloat(text).toLocaleString('en') : ''
+                    },
+                    () => {
+                      this.textInput.setNativeProps({ text: this.state.text });
+                    }
+                  );
                 }}
               />
             </View>
@@ -170,15 +311,27 @@ export default class LaunchScreen extends Component {
               alignItems: 'center'
             }}
           >
-            <Text
+            <TouchableOpacity
               style={{
-                color: 'white',
-                fontSize: 22,
-                fontFamily: 'SpaceMono-Regular'
+                height: Metrics.screenHeight * 0.08,
+                width: Metrics.screenWidth * 0.12,
+                justifyContent: 'center',
+                alignItems: 'center'
+              }}
+              onPress={() => {
+                this.animate(true);
               }}
             >
-              {this.state.target}
-            </Text>
+              <Text
+                style={{
+                  color: 'white',
+                  fontSize: 22,
+                  fontFamily: 'SpaceMono-Regular'
+                }}
+              >
+                {this.state.target}
+              </Text>
+            </TouchableOpacity>
             <View
               style={{
                 height: Metrics.screenHeight * 0.08,
@@ -241,7 +394,7 @@ export default class LaunchScreen extends Component {
                     alignItems: 'center'
                   }}
                   onPress={() => {
-                    this.setState({ base: prop.acro, showFeed: false }, () => {
+                    this.setState({ [`${activeCurrency}`]: prop.acro, convert: false }, () => {
                       this.getRates();
                     });
                   }}
@@ -266,7 +419,7 @@ export default class LaunchScreen extends Component {
                     alignItems: 'center'
                   }}
                   onPress={() => {
-                    this.setState({ base: prop.acro, showFeed: false }, () => {
+                    this.setState({ [`${activeCurrency}`]: prop.acro, convert: false }, () => {
                       this.getRates();
                     });
                   }}
